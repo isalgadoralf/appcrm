93926da09e82ec923259ca29d0e137778174b4df
Rafael Salgado Flores <rafael.salgado@tesabiz.com>
2024-09-21 11:14:37 -0400

    servico fabricante

---
+++ src/main/java/soe/solid/appcrm/controller/CtrFabricante.java
@@ -0,0 +1,32 @@
+package soe.solid.appcrm.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import soe.solid.appcrm.model.Producto;
+import soe.solid.appcrm.service.IProductoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+@RequiredArgsConstructor
+public class CtrFabricante {
+
+    private final IProductoService productoService;
+
+    @GetMapping
+    public List<Producto> listarProductos() throws Exception {
+        return productoService.listarProductos();
+
+    }
+    @GetMapping("/{id}")
+    public Producto readById(@PathVariable("id") Integer id   ) throws Exception {
+        return productoService.obtenerProductoPorId(id);
+
+    }
+    @PostMapping
+    public Producto save(@RequestBody  Producto producto   ) throws Exception {
+        return productoService.registrarProducto(producto);
+
+    }
+}
