57d89e767fd3ca621ac35d2ec37e2638de87c5bb
Rafael Salgado Flores <rafael.salgado@tesabiz.com>
2024-10-29 22:57:45 -0400

    ajuste en jenkisfile

--- src/main/java/soe/solid/appcrm/WebConfig.java
+++ src/main/java/soe/solid/appcrm/WebConfig.java
@@ -1,5 +1,6 @@
 package soe.solid.appcrm;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,10 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
+
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") 
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
