35a12091185311d1b0081612aaa88b9cba6f0cd3
Rafael Salgado Flores <rafael.salgado@tesabiz.com>
2024-11-04 21:44:41 -0400

    ajsute cros

--- src/main/java/soe/solid/appcrm/WebConfig.java
+++ src/main/java/soe/solid/appcrm/WebConfig.java
@@ -1,10 +1,7 @@
 package soe.solid.appcrm;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,43 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
 
-
+    @Value("${cors.allowed-origin}")
     private String allowedOrigin = "*";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println("allowedOrigin: " + allowedOrigin);
         registry.addMapping("/**")
-                .allowedOriginPatterns(allowedOrigin)
-                .allowedHeaders("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*")
-                .exposedHeaders("*")
-                .maxAge(3600);
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        // Permite cualquier origen
-        config.addAllowedOriginPattern("*");
-
-        // Permite todos los métodos HTTP
-        config.addAllowedMethod("*");
-
-        // Permite todas las cabeceras
-        config.addAllowedHeader("*");
-
-        // Expone todas las cabeceras
-        config.addExposedHeader("*");
-
-        // Tiempo de cache para la respuesta preflight
-        config.setMaxAge(3600L);
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
 }
