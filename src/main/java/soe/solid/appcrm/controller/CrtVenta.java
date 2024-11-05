0c64dd250f52cd4d9c56a2f8dcfbf30562908330
Rafael Salgado Flores <rafael.salgado@tesabiz.com>
2024-11-04 22:29:08 -0400

    get service venta

--- src/main/java/soe/solid/appcrm/controller/CrtVenta.java
+++ src/main/java/soe/solid/appcrm/controller/CrtVenta.java
@@ -4,13 +4,13 @@ package soe.solid.appcrm.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import soe.solid.appcrm.model.Fabricante;
 import soe.solid.appcrm.model.ResponseDto;
 import soe.solid.appcrm.model.Venta;
 import soe.solid.appcrm.service.spc.IVentaService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/venta")
@@ -18,7 +18,11 @@ import soe.solid.appcrm.service.spc.IVentaService;
 public class CrtVenta {
 
     private final IVentaService ventaService;
+    @GetMapping
+    public ResponseEntity<String> listAll()  {
+        return new ResponseEntity<>("Servicio de venta.....", HttpStatus.OK);
 
+    }
     @PostMapping("/save")
     public ResponseEntity<ResponseDto> save(@RequestBody Venta venta) {
         return new ResponseEntity<>(ventaService.registrar(venta), HttpStatus.OK);
